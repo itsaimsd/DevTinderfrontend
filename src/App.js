72462b988c1ede1453fd189995b674f6d0fbe6e8
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-20 23:25:28 +0530

    Starting frontend of DevTiner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React from "react"; // To define a scrollable section
-import TopHeader from "./components/Header/TopHeader";
-import Header from "./components/Header/Header";
-import HomePage from "./components/pages/HomePage";
-import Footer from "./components/Footer/Footer"; 
-
 function App() {
   return (
     <div>
-      <TopHeader />
-      <Header /> 
-      <HomePage /> 
-      <Footer />
+      <div className="text-3xl font-bold text-green-500">
+        Tailwind CSS is Working!
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 0.2s ease-in-out",
-        typing: "typing 3s steps(30) 1s infinite",
-      },
-      keyframes: {
-        fadeIn: {
-          from: { opacity: 0, transform: "translateY(-5px)" },
-          to: { opacity: 1, transform: "translateY(0)" },
-        },
-        typing: {
-          "0%": { width: "0%" },
-          "100%": { width: "76%" },
-        },
-        fontFamily: {
-          roboto: ["Roboto", "sans-serif"],
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
