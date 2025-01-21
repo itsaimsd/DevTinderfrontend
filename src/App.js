0b9f580fac5960b169a2fe594832b0a0e2d9285a
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-21 23:41:40 +0530

    Creating succesfully Tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
     <div>
-      <div className="text-3xl font-bold text-green-500">
+      <div className="text-3xl font-bold text-green-900">
+        Tailwind CSS is Working!
+      </div>      <div className="text-3xl font-bold text-green-900">
+        Tailwind CSS is Working!
+      </div>      <div className="text-3xl font-bold text-green-900">
         Tailwind CSS is Working!
       </div>
     </div>
